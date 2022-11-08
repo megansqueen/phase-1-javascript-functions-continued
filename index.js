df9ddc28c7a0befcc5586346c1b1bf42e5e7fe84
Megan <esler6724@gmail.com>
2022-11-07 23:24:43 -0800

    commit tree 245667696144168b8198aa30097768c0178ecf32

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = `go to the office`) {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(exclam = `*`) {
+    const compliment = function (emphatic = `special`) {
+        return `You are ${exclam}${emphatic}${exclam}!`;
+    }
+    return compliment;
+}
